7319fcdd394b8473c9d95d3b0bee1a65e1410c03
katia-kaci <katia.kaci.1@ens.etsmtl.ca>
2023-11-12 17:39:20 -0500

    pas important

--- Hnefatafl/src/Board.java
+++ Hnefatafl/src/Board.java
@@ -2,34 +2,11 @@ import java.util.*;
 
 class Board
 {
-	private int[][] board;
-	private int posKingX;
-	private int posKingY;
-	private Map<Integer, String> boardConversionRow = new HashMap<>();
 	private final int EMPTY = 0, CORNER = 1, BLACK = 2, RED = 4, KING = 5;
-	
-	private char[] rows = "ABCDEFGHIJKLM".toCharArray();
+	private int[][] board;
+	private int posKingX, posKingY;
+	private Map<Integer, String> boardConversionRow = new HashMap<>();
 
-	public int[][] getBoard() {
-		return board;
-	}
-
-	public int getPosKingX() {
-		return posKingX;
-	}
-
-	public void setPosKingX(int posKingX) {
-		this.posKingX = posKingX;
-	}
-
-	public int getPosKingY() {
-		return posKingY;
-	}
-
-	public void setPosKingY(int posKingY) {
-		this.posKingY = posKingY;
-	}
-	
 	/**
 	 * Initialise le plateau de la console
 	 * @param sBoard
@@ -50,25 +27,28 @@ class Board
 				y++;
 			}
 		}
-		for(int i=0; i<rows.length;i++) {
-			boardConversionRow.put(i, String.valueOf(rows[i]));
-		}
 		// Les quatres coins du board
 		this.board[0][0] = CORNER;
 		this.board[0][12] = CORNER;
 		this.board[12][0] = CORNER;
 		this.board[12][12] = CORNER;
+
+		char[] rows = "ABCDEFGHIJKLM".toCharArray();
+		for(int i=0; i<rows.length;i++) {
+			boardConversionRow.put(i, String.valueOf(rows[i]));
+		}
 	}
 
 	/**
 	 * Générer les coups possibles
+	 * @param aiPlayer ROUGE ou NOIR
 	 * @return la liste de coups possibles
 	 */
-	public ArrayList<Move> findPossibleMoves() {
+	public ArrayList<Move> findPossibleMoves(int aiPlayer) {
 		ArrayList<Move> possibleMoves = new ArrayList<>();
 		for(int i=0;i<board.length;i++) {
 			for(int j=0; j<board[i].length;j++) {
-				if(board[i][j] == RED) {
+				if(board[i][j] == aiPlayer) {
 					// vérifier en bas du pion
 					for(int row=i+1; row<13;row++) {
 						// si on rencontre un pion
@@ -98,47 +78,66 @@ class Board
 		}
 		return possibleMoves;
 	}
-		
+
 	public String printMove(Move m) {
 		return boardConversionRow.get(m.getRowStart())+""+m.getColStart()+" - "+boardConversionRow.get(m.getRowTarget())+""+m.getColTarget();
 	}
 
 	public int[][]  eliminerJetonAdverse(int couleurJoueur, int couleurAdverse, int colonneFin, int rangeeFin) {
-        int king = 5;
-        //change la couleur du king pour rouge si c est le joueur rouge qui a fait un movement pour manger
-        // afin qu un joueur rouge ne puisse pas manger un noir a laide du king.
-        if(couleurJoueur == 4) {
-            king = 4;
-        }
-        if (colonneFin < 11) {
-            if (this.board[colonneFin + 1][rangeeFin] == couleurAdverse) {
-                if (this.board[colonneFin + 2][rangeeFin] == couleurJoueur || this.board[colonneFin + 2][rangeeFin] == 1 || this.board[colonneFin + 2][rangeeFin] == king || (colonneFin + 2 == 6 && rangeeFin == 6)) {
-                    this.board[colonneFin + 1][rangeeFin] = 0;
-                }
-            }
-        }
-        if (rangeeFin < 11) {
-            if (this.board[colonneFin][rangeeFin + 1] == couleurAdverse) {
-                if (this.board[colonneFin][rangeeFin + 2] == couleurJoueur || this.board[colonneFin][rangeeFin + 2] == 1 || this.board[colonneFin][rangeeFin + 2] == king || (colonneFin == 6 && rangeeFin + 2 == 6)) {
-                    this.board[colonneFin][rangeeFin + 1] = 0;
-                }
-            }
-        }
-        if (colonneFin > 1) {
-            if (this.board[colonneFin - 1][rangeeFin] == couleurAdverse) {
-                if (this.board[colonneFin - 2][rangeeFin] == couleurJoueur || this.board[colonneFin - 2][rangeeFin] == 1 || this.board[colonneFin - 2][rangeeFin] == king || (colonneFin - 2 == 6 && rangeeFin == 6)) {
-                    this.board[colonneFin - 1][rangeeFin] = 0;
-                }
-            }
-        }
-        if (rangeeFin > 1) {
-            if (this.board[colonneFin][rangeeFin - 1] == couleurAdverse) {
-                if (this.board[colonneFin][rangeeFin - 2] == couleurJoueur || this.board[colonneFin][rangeeFin - 2] == 1 || this.board[colonneFin][rangeeFin - 2] == king || (colonneFin == 6 && rangeeFin - 2 == 6)) {
-                    this.board[colonneFin][rangeeFin - 1] = 0;
-                }
-            }
-        }
-        return this.board;
-    }
+		int king = 5;
+		//change la couleur du king pour rouge si c est le joueur rouge qui a fait un movement pour manger
+		// afin qu un joueur rouge ne puisse pas manger un noir a laide du king.
+		if(couleurJoueur == 4) {
+			king = 4;
+		}
+		if (colonneFin < 11) {
+			if (this.board[colonneFin + 1][rangeeFin] == couleurAdverse) {
+				if (this.board[colonneFin + 2][rangeeFin] == couleurJoueur || this.board[colonneFin + 2][rangeeFin] == 1 || this.board[colonneFin + 2][rangeeFin] == king || (colonneFin + 2 == 6 && rangeeFin == 6)) {
+					this.board[colonneFin + 1][rangeeFin] = 0;
+				}
+			}
+		}
+		if (rangeeFin < 11) {
+			if (this.board[colonneFin][rangeeFin + 1] == couleurAdverse) {
+				if (this.board[colonneFin][rangeeFin + 2] == couleurJoueur || this.board[colonneFin][rangeeFin + 2] == 1 || this.board[colonneFin][rangeeFin + 2] == king || (colonneFin == 6 && rangeeFin + 2 == 6)) {
+					this.board[colonneFin][rangeeFin + 1] = 0;
+				}
+			}
+		}
+		if (colonneFin > 1) {
+			if (this.board[colonneFin - 1][rangeeFin] == couleurAdverse) {
+				if (this.board[colonneFin - 2][rangeeFin] == couleurJoueur || this.board[colonneFin - 2][rangeeFin] == 1 || this.board[colonneFin - 2][rangeeFin] == king || (colonneFin - 2 == 6 && rangeeFin == 6)) {
+					this.board[colonneFin - 1][rangeeFin] = 0;
+				}
+			}
+		}
+		if (rangeeFin > 1) {
+			if (this.board[colonneFin][rangeeFin - 1] == couleurAdverse) {
+				if (this.board[colonneFin][rangeeFin - 2] == couleurJoueur || this.board[colonneFin][rangeeFin - 2] == 1 || this.board[colonneFin][rangeeFin - 2] == king || (colonneFin == 6 && rangeeFin - 2 == 6)) {
+					this.board[colonneFin][rangeeFin - 1] = 0;
+				}
+			}
+		}
+		return this.board;
+	}
 
+	public int[][] getBoard() {
+		return board;
+	}
+
+	public int getPosKingX() {
+		return posKingX;
+	}
+
+	public void setPosKingX(int posKingX) {
+		this.posKingX = posKingX;
+	}
+
+	public int getPosKingY() {
+		return posKingY;
+	}
+
+	public void setPosKingY(int posKingY) {
+		this.posKingY = posKingY;
+	}
 }
--- Hnefatafl/src/Client.java
+++ Hnefatafl/src/Client.java
@@ -5,105 +5,29 @@ import java.util.*;
 class Client {
 
 	static Board board;
+	static BufferedInputStream input;
+	static BufferedOutputStream output;
+	static BufferedReader console;
+	static Socket MyClient;
+	static int aiPlayer;
+
+	static final int EMPTY = 0, CORNER = 1, BLACK = 2, RED = 4, KING = 5;
 
 	public static void main(String[] args) {
-		final char ROUGE = '1', NOIR = '2', PROCHAIN = '3', INVALIDE = '4', TERMINER = '5';
-
-		Socket MyClient;
-		BufferedInputStream input;
-		BufferedOutputStream output;
-
 		try {
 			MyClient = new Socket("localhost", 8888);
 			input    = new BufferedInputStream(MyClient.getInputStream());
 			output   = new BufferedOutputStream(MyClient.getOutputStream());
-			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-
-			while(true){
+			console = new BufferedReader(new InputStreamReader(System.in));
+			while(true) {
 				char cmd = (char)input.read();
-				System.out.println("Commande: "+cmd);
-
-				// Debut de la partie en joueur blanc (rouge)	
-				if(cmd == ROUGE){
-					byte[] aBuffer = new byte[1024];
-					int size = input.available();
-					//System.out.println("size " + size);
-					input.read(aBuffer,0,size);
-					String s = new String(aBuffer).trim();
-					//					System.out.println("chaine du plateau: "+s);
-					board = new Board(s);
-					showBoard();
-
-					System.out.println("Nouvelle partie! Vous jouer blanc, entrez votre premier coup : ");
-					String move = null;
-					move = console.readLine();
-					output.write(move.getBytes(),0,move.length());
-					output.flush();
-				}
-
-				// Debut de la partie en joueur Noir
-				if(cmd == NOIR){
-					System.out.println("Nouvelle partie! Vous jouer noir, attendez le coup des blancs");
-					byte[] aBuffer = new byte[1024];
-
-					int size = input.available();
-					//System.out.println("size " + size);
-					input.read(aBuffer,0,size);
-					String s = new String(aBuffer).trim();
-					//					System.out.println(s);
-					board = new Board(s);
-				}
-
-				// Le serveur demande le prochain coup
-				// Le message contient aussi le dernier coup joue.
-				if(cmd == PROCHAIN){
-					byte[] aBuffer = new byte[16];
-					int size = input.available();
-					System.out.println("size :" + size);
-					input.read(aBuffer,0,size);
-
-					String s = new String(aBuffer);
-					System.out.println("Dernier coup :"+ s);
-					
-					
-					// A ENLEVER : !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-					ArrayList<Move> moves = board.findPossibleMoves();
-					String possiblesMoves= "";
-					for(int i=0;i< moves.size();i++) {
-						possiblesMoves+=board.printMove(moves.get(i))+" / ";
-					}
-					System.out.println("Coups possibles : "+possiblesMoves);
-					
-					
-					
-					System.out.println("Entrez votre coup : ");
-					String move = null;
-					move = console.readLine();
-					output.write(move.getBytes(),0,move.length());
-					output.flush();
-				}
-
-				// Le dernier coup est invalide
-				if(cmd == INVALIDE){
-					System.out.println("Coup invalide, entrez un nouveau coup : ");
-					String move = null;
-					move = console.readLine();
-					output.write(move.getBytes(),0,move.length());
-					output.flush();
-				}
-
-				// La partie est terminée
-				if(cmd == TERMINER){
-					byte[] aBuffer = new byte[16];
-					int size = input.available();
-					input.read(aBuffer,0,size);
-					String s = new String(aBuffer);
-					System.out.println("Partie Terminé. Le dernier coup joué est: "+s);
-					String move = null;
-					move = console.readLine();
-					output.write(move.getBytes(),0,move.length());
-					output.flush();
-				}
+				System.out.println("\nCommande: "+cmd);
+				if(cmd == '1') startGameAsRed();
+				if(cmd == '2') startGameAsBlack();
+				if(cmd == '3') nextMove();
+				if(cmd == '4')invalidMove();
+				if(cmd == '5')endGame();
+				System.out.println("\nle ai est: "+aiPlayer);
 			}
 		}
 		catch (IOException e) {
@@ -111,10 +35,88 @@ class Client {
 		}
 	}
 
+	private static void startGameAsRed() throws IOException {
+		byte[] aBuffer = new byte[1024];
+		int size = input.available();
+		//System.out.println("size " + size);
+		input.read(aBuffer,0,size);
+		String s = new String(aBuffer).trim();
+		// System.out.println("chaine du plateau: "+s);
+		board = new Board(s);
+		aiPlayer = RED;
+		showBoard();
+
+		System.out.print("Nouvelle partie! Vous jouer rouge, entrez votre premier coup : ");
+		String move = null;
+		move = console.readLine();
+		output.write(move.getBytes(),0,move.length());
+		output.flush();
+	}
+
+	private static void startGameAsBlack() throws IOException {
+		System.out.println("Nouvelle partie! Vous jouer noir, attendez le coup des rouges");
+		byte[] aBuffer = new byte[1024];
+		int size = input.available();
+		// System.out.println("size " + size);
+		input.read(aBuffer,0,size);
+		String s = new String(aBuffer).trim();
+		// System.out.println(s);
+		board = new Board(s);
+		aiPlayer = BLACK;
+	}
+
+	private static void nextMove() throws IOException {
+		// Le serveur demande le prochain coup
+		// Le message contient aussi le dernier coup joue.
+		byte[] aBuffer = new byte[16];
+		int size = input.available();
+		//		System.out.println("size :" + size);
+		input.read(aBuffer,0,size);
+		String s = new String(aBuffer);
+		System.out.println("Dernier coup :"+ s);
+
+
+		// A ENLEVER : !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		ArrayList<Move> moves = board.findPossibleMoves(aiPlayer);
+		String possiblesMoves= "";
+		for(int i=0;i< moves.size();i++) {
+			possiblesMoves+=board.printMove(moves.get(i))+" / ";
+		}
+		System.out.println("Coups possibles pour "+aiPlayer+": "+possiblesMoves);
+		// A ENLEVER : !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+		System.out.print("Entrez votre coup : ");
+		String move = null;
+		move = console.readLine();
+		output.write(move.getBytes(),0,move.length());
+		output.flush();
+	}
+
+	private static void invalidMove() throws IOException {
+		System.out.print("Coup invalide, entrez un nouveau coup : ");
+		String move = null;
+		move = console.readLine();
+		output.write(move.getBytes(),0,move.length());
+		output.flush();
+	}
+
+	private static void endGame() throws IOException {
+		byte[] aBuffer = new byte[16];
+		int size = input.available();
+		input.read(aBuffer,0,size);
+		String s = new String(aBuffer);
+		System.out.println("Partie Terminé. Le dernier coup joué est: "+s);
+		String move = null;
+		move = console.readLine();
+		output.write(move.getBytes(),0,move.length());
+		output.flush();
+	}
+
 	/**
 	 * Imprime le tableau dans la console
 	 */
-	public static void showBoard() {
+	private static void showBoard() {
 		System.out.println();
 		for (int x = 0; x < 13; x++) {
 			System.out.print(String.format("%3d", 13 - x) + " |");
